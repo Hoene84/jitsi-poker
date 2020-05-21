afcd86d0b399c41e4fc6e320b281609618f8452a
Simon Honegger <simon.honegger@gmail.com>
2020-05-21 10:50:18 +0200

    chore: add eslint-plugin-only-warn
    this avoids that the IDE shows up lint problems as errors.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,9 @@ module.exports = {
     'extends': [
         'eslint-config-jitsi'
     ],
+    "plugins": [
+        "only-warn"
+    ],
     'rules': {
         'require-jsdoc': 'off',
         '@react/jsx-no-bind': 'off'
