50d9cef71f76bf65891ddae3709c19d731f62faa
Hoene84 <simon.honegger@gmail.com>
2020-06-04 09:43:14 +0000

    Deploying to gh-pages from  @ 07c759ea4de8a37d74581470b9a3fb3ab64da223 🚀

--- config.js
+++ config.js
@@ -1,22 +1,20 @@
-/* eslint-disable */
 var subdomain = "";
 if (subdomain) {
     subdomain = subdomain.substr(0,subdomain.length-1).split('.').join('_').toLowerCase() + '.';
 }
 var config = {
     hosts: {
-        domain: 'alpha.jitsi.net',
+        domain: 'beta.meet.jit.si',
 
-        muc: 'conference.'+subdomain+'alpha.jitsi.net', // FIXME: use XEP-0030
-        focus: 'focus.alpha.jitsi.net',
+        muc: 'conference.'+subdomain+'beta.meet.jit.si', // FIXME: use XEP-0030
+        focus: 'focus.beta.meet.jit.si',
     },
     disableSimulcast: false,
-    enableRemb: false,
+    enableRemb: true,
     enableTcc: true,
     resolution: 720,
     constraints: {
         video: {
-            aspectRatio: 16 / 9,
             height: {
                 ideal: 720,
                 max: 720,
@@ -29,54 +27,46 @@ var config = {
             }
         }
     },
-    externalConnectUrl: '//alpha.jitsi.net/http-pre-bind',
     analytics: {
+        whiteListedEvents: [ 'conference.joined', 'page.reload.scheduled', 'rejoined', 'transport.stats' ],
     },
-    p2pStunServers: [
-        { urls: "stun:stun.l.google.com:19302" },
-        { urls: "stun:stun1.l.google.com:19302" },
-        { urls: "stun:stun2.l.google.com:19302" }
-    ],
-    //enableP2P: false, // flag to control P2P connections
+    enableP2P: true, // flag to control P2P connections
     // New P2P options
     p2p: {
-        enabled: false,
+        enabled: true,
         preferH264: true,
         disableH264: true,
-        useStunTurn: true, // use XEP-0215 to fetch STUN and TURN server for the P2P connection
-        stunServers: [
-            { urls: "stun:stun.l.google.com:19302" },
-            { urls: "stun:stun1.l.google.com:19302" },
-            { urls: "stun:stun2.l.google.com:19302" }
-        ]
+        useStunTurn: true // use XEP-0215 to fetch STUN and TURN servers for the P2P connection
     },
-    useStunTurn: true, // use XEP-0215 to fetch STUN and TURN server for the JVB connection
+    useStunTurn: true, // use XEP-0215 to fetch TURN servers for the JVB connection
     useIPv6: false, // ipv6 support. use at your own risk
     useNicks: false,
-    bosh: '//alpha.jitsi.net/http-bind', // FIXME: use xep-0156 for that
+    bosh: '//beta.meet.jit.si/http-bind', // FIXME: use xep-0156 for that
+    websocket: 'wss://beta.meet.jit.si/xmpp-websocket', // FIXME: use xep-0156 for that
 
-    //etherpad_base: 'https://alpha.jitsi.net/etherpad/p/',
+
     clientNode: 'http://jitsi.org/jitsimeet', // The name of client node advertised in XEP-0115 'c' stanza
     //deprecated desktop sharing settings, included only because older version of jitsi-meet require them
     desktopSharing: 'ext', // Desktop sharing method. Can be set to 'ext', 'webrtc' or false to disable.
-    chromeExtensionId: 'pgplkhlodppdcflanmijbnpgpjkegfgc', // Id of desktop streamer Chrome extension
+    chromeExtensionId: 'kglhbbefdnlheedjiejgomgmfplipfeb', // Id of desktop streamer Chrome extension
     desktopSharingSources: ['screen', 'window'],
+    enableCalendarIntegration: true,
     //new desktop sharing settings
-    desktopSharingChromeExtId: 'pgplkhlodppdcflanmijbnpgpjkegfgc', // Id of desktop streamer Chrome extension
+    desktopSharingChromeExtId: 'kglhbbefdnlheedjiejgomgmfplipfeb', // Id of desktop streamer Chrome extension
     desktopSharingChromeDisabled: false,
     desktopSharingChromeSources: ['screen', 'window', 'tab'],
-    desktopSharingChromeMinExtVersion: '0.1.3', // Required version of Chrome extension
-    desktopSharingFirefoxExtId: "jidesha@alpha.jitsi.net",
-    desktopSharingFirefoxDisabled: true,
+    desktopSharingChromeMinExtVersion: '0.2.6.2', // Required version of Chrome extension
+    desktopSharingFirefoxExtId: "",
+    desktopSharingFirefoxDisabled: false,
     desktopSharingFirefoxMaxVersionExtRequired: '0',
     desktopSharingFirefoxExtensionURL: "",
     useRoomAsSharedDocumentName: false,
-    enableLipSync: false, // Disables lip-sync everywhere.
+    enableLipSync: false,
     disableRtx: false, // Enables RTX everywhere
     enableRtpStats: false, // Enables RTP stats processing
+    enableScreenshotCapture: false,
     enableStatsID: true,
     openBridgeChannel: 'websocket', // One of true, 'datachannel', or 'websocket'
-    //openBridgeChannel: 'datachannel', // One of true, 'datachannel', or 'websocket'
     channelLastN: -1, // The default value of the channel attribute last-n.
     minHDHeight: 540,
     startBitrate: "800",
@@ -84,15 +74,21 @@ var config = {
     useRtcpMux: true,
     useBundle: true,
     disableSuspendVideo: true,
-    stereo: true,
+    stereo: false,
     forceJVB121Ratio:  -1,
     enableTalkWhileMuted: true,
 
-    hiddenDomain: 'recorder.alpha.jitsi.net',
-    transcribingEnabled: false,
+    enableNoAudioDetection: true,
+
+    enableNoisyMicDetection: true,
+
+    enableClosePage: true,
+
+    hiddenDomain: 'recorder.beta.meet.jit.si',
+    transcribingEnabled: true,
     enableRecording: true,
     liveStreamingEnabled: true,
-    fileRecordingsEnabled: false,
+    fileRecordingsEnabled: true,
     fileRecordingsServiceEnabled: false,
     fileRecordingsServiceSharingEnabled: false,
     requireDisplayName: false,
@@ -100,10 +96,7 @@ var config = {
     enableWelcomePage: true,
     isBrand: false,
     logStats: false,
-// To enable sending statistics to callstats.io you should provide Applicaiton ID and Secret.
-    callStatsID: "294674397",//Application ID for callstats.io API
-    callStatsSecret: "9IJJTtOdheZs:MHov7tz0Gc3h/6NYXiNVCqA1tpTmKPH0AdXTYtAKVRY=",//Secret for callstats.io API
-    dialInNumbersUrl: 'https://api.jitsi.net/phoneNumberList',
+    dialInNumbersUrl: 'https://web-cdn.jitsi.net/beta/phoneNumberList.json',
     dialInConfCodeUrl:  'https://api.jitsi.net/conferenceMapper',
 
     dialOutCodesUrl:  'https://api.jitsi.net/countrycodes',
@@ -115,10 +108,12 @@ var config = {
     startAudioMuted: 9,
     startVideoMuted: 9,
     enableUserRolesBasedOnToken: false,
+    enableLayerSuspension: false,
+    feedbackPercentage: 100,
     hepopAnalyticsUrl: "",
     hepopAnalyticsEvent: {
         product: "lib-jitsi-meet",
-        subproduct: "alpha",
+        subproduct: "beta-meet-jit-si",
         name: "jitsi.page.load.failed",
         action: "page.load.failed",
         actionSubject: "page.load",
@@ -128,29 +123,33 @@ var config = {
             type: "operational",
             source: 'page.load'
         },
-        server: "alpha.jitsi.net"
+        server: "beta.meet.jit.si"
     },
     deploymentInfo: {
-        environment: 'alpha',
-        envType: 'dev',
-        releaseNumber: '',
-        shard: 'all',
-        region: 'us-west-2',
-        userRegion: '',
-        crossRegion: (!'' || 'us-west-2' === 'default_region') ? 0 : 1
+        environment: 'beta-meet-jit-si',
+        envType: 'stage',
+        releaseNumber: '700',
+        shard: 'beta-eu-west-2b-s5',
+        region: 'eu-west-2',
+        userRegion: 'eu-west-2',
+        crossRegion: (!'eu-west-2' || 'eu-west-2' === 'eu-west-2') ? 0 : 1
     },
     rttMonitor: {
         enabled: false,
         initialDelay: 30000,
         getStatsInterval: 10000,
         analyticsInterval: 60000,
-        stunServers: {"ap-se-1": "all-ap-se-1-turn.jitsi.net:443", "ap-se-2": "all-ap-se-2-turn.jitsi.net:443", "eu-central-1": "all-eu-central-1-turn.jitsi.net:443", "eu-west-1": "all-eu-west-1-turn.jitsi.net:443", "us-east-1": "all-us-east-1-turn.jitsi.net:443", "us-west-2": "all-us-west-2-turn.jitsi.net:443"}
+        stunServers: {"us-east-1": "all-us-east-1-turn.jitsi.net:443", "ap-se-2": "all-ap-se-2-turn.jitsi.net:443", "ap-se-1": "all-ap-se-1-turn.jitsi.net:443", "us-west-2": "all-us-west-2-turn.jitsi.net:443", "eu-central-1": "all-eu-central-1-turn.jitsi.net:443", "eu-west-1": "all-eu-west-1-turn.jitsi.net:443"}
+    },
+    e2eping: {
+        pingInterval: -1
     },
     abTesting: {
     },
     testing: {
+        capScreenshareBitrate: 1,
         octo: {
-            probability: 0
+            probability: 1
         }
     }
 };
