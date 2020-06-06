12413eacf5d27da95b8abcc89c3feb34dc0236bd
Simon Honegger <simon.honegger@gmail.com>
2020-06-06 14:37:16 +0200

    style: add actual poker hand to control panel

--- react/features/poker/components/ControlPanel.js
+++ react/features/poker/components/ControlPanel.js
@@ -3,17 +3,19 @@
 import React, { Component } from 'react';
 import { connect } from '../../base/redux';
 import { translate } from '../../base/i18n';
-import type { Action } from '../types';
+import type { Action, Card as CardType } from '../types';
 import Tooltip from '@atlaskit/tooltip';
 import { CALL, CHECK, FOLD, GIVE_CARDS, RAISE } from '../actionTypes';
 import { call, check, fold, giveCards, raise } from '../actions';
-import { pokerActionTypes } from '../functions';
+import { cards, pokerActionTypes } from '../functions';
 import { getParticipantDisplayName } from '../../base/participants';
+import { SYMBOLS } from '../constants';
 
 export type Props = {
     participantId: string,
     dispatch: Function,
     t: Function,
+    _cards: Array<CardType>,
     _actions: Array<Action>,
 }
 
@@ -42,20 +44,61 @@ class ControlPanel extends Component<Props, State> {
 
         return (
             <div className = 'poker-control-panel'>
-                {_actions.map(action => (<button
-                    aria-label = { t(`poker.action.${action}`) }
-                    className = 'poker-button'
-                    key = { action }
-                    /* eslint-disable-next-line react/jsx-no-bind */
-                    onClick = { e => this._onAction(e, action) }>
-                    <Tooltip content = { t(`poker.action.${action}`) }>
-                        <div className = { 'poker-action' }>
-                            {t(`poker.action.${action}`)}
-                        </div>
-                    </Tooltip>
-                </button>))}
+                <div className = 'poker-buttons'>
+                    {_actions.map(action => (<button
+                        aria-label = { t(`poker.action.${action}`) }
+                        className = 'poker-button'
+                        key = { action }
+                        /* eslint-disable-next-line react/jsx-no-bind */
+                        onClick = { e => this._onAction(e, action) }>
+                        <Tooltip content = { t(`poker.action.${action}`) }>
+                            <div className = { 'poker-action' }>
+                                {t(`poker.action.${action}`)}
+                            </div>
+                        </Tooltip>
+                    </button>))}
+                </div>
+                {this.props._cards.length === 2
+                && <div className = 'poker-hand'>
+                    <img src = 'images/hand_cropped.png' />
+                    <div className = 'card card-one'>
+                        <span className = { this._symbolClasses(this.props._cards[0]) }>
+                            {SYMBOLS[this.props._cards[0]?.symbol]}
+                        </span>
+                        <span className = 'suite'>{this._suite(this.props._cards[0])}</span>
+                    </div>
+                    <div className = 'card card-two'>
+                        <span className = { this._symbolClasses(this.props._cards[1]) }>
+                            {SYMBOLS[this.props._cards[1]?.symbol]}
+                        </span>
+                        <span className = 'suite'>{this._suite(this.props._cards[1])}</span>
+                    </div>
+                </div>}
             </div>
         );
+    }
+
+    _suite(card: CardType) {
+        if (card.suit === 'club') {
+            return <span>&clubs;</span>;
+        }
+        if (card.suit === 'diamond') {
+            return <span>&diams;</span>;
+        }
+        if (card.suit === 'heart') {
+            return <span>&hearts;</span>;
+        }
+        if (card.suit === 'spade') {
+            return <span>&spades;</span>;
+        }
+    }
+
+    _symbolClasses(card: CardType) {
+        if (card.suit === 'heart' || card.suit === 'diamond') {
+            return 'symbol red';
+        }
+
+        return 'symbol black';
     }
 
     _onAction: (Object, string) => void;
@@ -88,7 +131,8 @@ export function _mapStateToProps(state: Object, ownProps: Props) {
 
 
     return {
-        _actions: pokerActionTypes(state, nick)
+        _actions: pokerActionTypes(state, nick),
+        _cards: cards(state, nick)
     };
 }
 
