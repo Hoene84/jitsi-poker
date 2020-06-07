25a91665a4a94a883fb48a7a2792b589a7368a06
Simon Honegger <simon.honegger@gmail.com>
2020-06-07 19:11:16 +0200

    fix: do not allow actions before cards

--- react/features/poker/logic/canDo.js
+++ react/features/poker/logic/canDo.js
@@ -13,21 +13,25 @@ export function canGiveCards(state: APokerState, nick: string): ?Action {
 
 export function canCheck(state: APokerState, nick: string): ?Action {
     return _isCurrentPlayer(state, nick)
+    && countCards(state, nick) === 2
     && currentPlayer(state)?.bet === state.common.game.round.bet ? CHECK : null;
 }
 
 export function canCall(state: APokerState, nick: string): ?Action {
     return _isCurrentPlayer(state, nick)
+    && countCards(state, nick) === 2
     && (currentPlayer(state)?.bet || 0) < state.common.game.round.bet ? CALL : null;
 }
 
 export function canRaise(state: APokerState, nick: string): ?Action {
     return _isCurrentPlayer(state, nick)
+    && countCards(state, nick) === 2
     && state.common.game.round.currentPlayer !== state.common.game.round.bettingRound.raisePlayer ? RAISE : null;
 }
 
 export function canFold(state: APokerState, nick: string): ?Action {
     return _isCurrentPlayer(state, nick)
+    && countCards(state, nick) === 2
     && !currentPlayer(state)?.fold ? FOLD : null;
 }
 
